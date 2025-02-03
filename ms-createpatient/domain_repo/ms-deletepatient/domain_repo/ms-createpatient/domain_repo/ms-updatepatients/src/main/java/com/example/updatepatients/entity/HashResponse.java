9733abf3262bb5e08a2f3a84a9a7935f8a0364af
DavidSebas20 <davidsebastian20@outlook.com>
2025-02-03 19:12:47 +0000

    :bug: feat(workflow): update sync workflow to exclude domain_repo during rsync and improve commit message retrieval

--- ms-createpatient/domain_repo/ms-deletepatient/domain_repo/ms-createpatient/domain_repo/ms-updatepatients/src/main/java/com/example/updatepatients/entity/HashResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.updatepatients.entity;
-
-public class HashResponse {
-    private String hash;
-
-    // Getters y Setters
-    public String getHash() { return hash; }
-    public void setHash(String hash) { this.hash = hash; }
-}
--- ms-createpatient/domain_repo/ms-deletepatient/domain_repo/ms-createpatient/domain_repo/ms-updatepatients/src/main/java/com/example/updatepatients/entity/VerifyResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.updatepatients.entity;
-
-public class VerifyResponse {
-    private boolean match;
-
-    // Getters y Setters
-    public boolean isMatch() { return match; }
-    public void setMatch(boolean match) { this.match = match; }
-}
